21ae7a0c79d36b2ca38c92f8bcc9e3158c2ef5c5
JhonFiUNFV <47376395+JhonFiUNFV@users.noreply.github.com>
2020-12-20 18:37:00 -0500

    Creando Paquete

---
+++ R/BCRPR.R
@@ -0,0 +1,78 @@
+#' Trabaja con informacion estadistica del Peru. Creado por BEST
+#'
+#' @name INEIR
+#' @docType package
+
+NULL
+
+#' Agua Publica: Agua Publica a nivel de Departamento del Peru.
+#'
+#' Agua Publica: Agua Publica a nivel de Departamento del Peru.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name aguapublica
+#' @usage data(aguapublica)
+#' @format Base de datos con la informacion del agua publica de los departamentos del Peru.
+#'
+NULL
+
+#' Alcantarillado: Alcantarillado a nivel de Departamento del Peru.
+#'
+#' Alcantarillado: Alcantarillado a nivel de Departamento del Peru.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name alcantarillado
+#' @usage data(alcantarillado)
+#' @format Base de datos con la informacion del alcantarillado de los departamentos del Peru.
+#'
+NULL
+
+#' Electricidad: Electricidad a nivel de Departamento del Peru.
+#'
+#' Electricidad: Electricidad a nivel de Departamento del Peru.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name electrico
+#' @usage data(electrico)
+#' @format Base de datos con la informacion de la electricidad de los departamentos del Peru.
+#'
+NULL
+
+#' Informalidad: Informalidad a nivel de Departamento del Peru.
+#'
+#' Informalidad: Informalidad a nivel de Departamento del Peru.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name informalidad
+#' @usage data(informalidad)
+#' @format Base de datos con la informacion de la informalidad de los departamentos del Peru.
+#'
+NULL
+
+#' PBI 2007: PBI del ano 2007 a nivel de Departamento del Peru.
+#'
+#' PBI 2007: PBI del ano 2007 a nivel de Departamento del Peru.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name PBI2007
+#' @usage data(PBI2007)
+#' @format Base de datos con la informacion del PBI del ano 2007 de los departamentos del Peru.
+#'
+NULL
+
+#' Pobreza: Pobreza a nivel de Departamento del Peru.
+#'
+#' Pobreza: Pobreza a nivel de Departamento del Peru.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name pobreza
+#' @usage data(pobreza)
+#' @format Base de datos con la informacion de la pobreza de los departamentos del Peru.
+#'
+NULL
